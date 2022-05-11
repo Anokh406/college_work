53e397f0831824b528b60cd1e2480a56f7242e7b
Anokh406 <95044585+Anokh406@users.noreply.github.com>
2022-05-11 23:08:43 +0530

    friend function in cpp

---
+++ cpp/friend.cpp
@@ -0,0 +1,30 @@
+#include "iostream"
+using namespace std;
+class Y;//forward declartion
+class X{
+    int data;
+    public:
+    void setvalue(int value){
+        data=value;
+    }
+    friend void add(X ,Y );
+};
+class Y{
+    int num;
+    friend void add(X ,Y );
+    public:
+    void setvalue(int val){
+        num=val;
+    }
+};
+void add(X o1,Y o2){
+    cout << "Suming the object of X and Y :" <<o1.data+o2.num<<endl;
+}
+ int main() {
+    X a;
+    Y b;
+    a.setvalue(5);
+    b.setvalue(4);
+    add(a,b);
+    return 0;
+}
